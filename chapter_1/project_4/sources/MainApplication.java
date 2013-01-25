e987fe637d40cec75bc141fed8ddccf71941d4e9
alexkonkin <alexkonkin@b8128056-237a-151d-1e11-175cfe163b60>
2013-01-25 15:17:12 +0000

    Calendar Seeker: the source code that implements the functionality of this project has been added to the repository

---
+++ chapter_1/project_4/sources/MainApplication.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+import com.globallogic.javase.*;
+
+
+public class MainApplication{
+	public static void main(String[] args) {
+	System.out.println("--==< program started >==--\n");
+	Scanner in = new Scanner(System.in);
+	System.out.println("Input the Date (for example 10 October 2010) : ");
+	String inputDate = String.valueOf(in.nextLine());
+	
+	CalendarSeeker varCalendarSeeker = new CalendarSeeker (inputDate);
+	varCalendarSeeker.DoCalculation();
+
+	System.out.println("\n--==< program stopped >==--");
+ }
+}
+
+
+
+
+
+
+
+
+
