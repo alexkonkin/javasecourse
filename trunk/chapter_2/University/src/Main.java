030171552f5482e5ce8aa5d19c6b0548e982af78
alexkonkin <alexkonkin@b8128056-237a-151d-1e11-175cfe163b60>
2013-02-20 16:56:13 +0000

    University: update of the source code

--- trunk/chapter_2/University/src/Main.java
+++ trunk/chapter_2/University/src/Main.java
@@ -1,6 +1,27 @@
+import com.globallogic.javase.university.businessObjects.*;
+import com.globallogic.javase.university.staff.*;
+
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("--==<The program is started>==--");
+
+        Teacher[] teachersTeam = new Teacher[3];
+        for(int i = 0; i < teachersTeam.length ; i++)
+            teachersTeam[i] = new Teacher(i);
+
+        Lesson[] lessonsStorage = new Lesson[5];
+        for(int i = 0; i < lessonsStorage.length ; i++)
+            lessonsStorage[i] = new Lesson(i,new String("Lesson "+(i+1)),new String("Lesson "+(i+1)+" info"));
+
+        Group[] groupsStorage = new Group[3];
+        for(int i = 0; i < groupsStorage.length ; i++)
+            groupsStorage[i] = new Group(i,new String("Group "+(i+1)));
+
+
+
+
     }
 }
--- trunk/chapter_2/University/src/com/globallogic/javase/tests/LessonTest.java
+++ trunk/chapter_2/University/src/com/globallogic/javase/tests/LessonTest.java
@@ -1,6 +1,10 @@
-package com.globallogic.javase.university.businessObjects;
+package com.globallogic.javase.tests;
 
+import com.globallogic.javase.university.businessObjects.Lesson;
+import org.junit.After;
+import static org.junit.Assert.*;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -15,34 +19,35 @@ public class LessonTest {
     Lesson aLessonNoArgs = new Lesson();
     Lesson aLessonThreeArgs = new Lesson(1,"Math","Mathematics lesson");
 
-    //Let's check the lesson object that was created via default constructor
+    //default constructor
     @Test
     public void testSetLessonId() throws Exception {
-
+        assertEquals(0,(int)aLessonNoArgs.getLessonId());
     }
 
     @Test
     public void testSetLessonName() throws Exception {
-
+        assertEquals("-",aLessonNoArgs.getLessonName());
     }
 
     @Test
     public void testSetLessonDesciption() throws Exception {
-
+        assertEquals("-",aLessonNoArgs.getLessonDesciption());
     }
 
+    //constructor with 3 arguments
     @Test
     public void testGetLessonId() throws Exception {
-
+        assertEquals(1,(int)aLessonThreeArgs.getLessonId());
     }
 
     @Test
     public void testGetLessonName() throws Exception {
-
+        assertEquals("Math",aLessonThreeArgs.getLessonName());
     }
 
     @Test
     public void testGetLessonDesciption() throws Exception {
-
+        assertEquals("Mathematics lesson",aLessonThreeArgs.getLessonDesciption());
     }
 }
