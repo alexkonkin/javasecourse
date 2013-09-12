634224c8cd1a7643078fdc4314adec3801f2ad87
alexkonkin@gmail.com <alexkonkin@gmail.com@b8128056-237a-151d-1e11-175cfe163b60>
2013-09-12 16:19:06 +0000

    BooksXmlProcessor: 1) xsd schema has been created from the books.xml file                    2) java classes have been generated for books.xml file                    3) java class that incorporates functionality that allows to work with books hierarchy has been created                    4) XML unit tests have been added to the project

---
+++ trunk/java_ee/books/src/Main.java
@@ -0,0 +1,28 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: oleksiy.konkin
+ * Date: 8/30/13
+ * Time: 10:21 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+import com.globallogic.javaee.books.*;
+import com.sun.org.apache.xerces.internal.jaxp.datatype.XMLGregorianCalendarImpl;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.io.*;
+import java.math.BigInteger;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.GregorianCalendar;
+import java.util.Properties;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println ("The program is started");
+    }
+}
