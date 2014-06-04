635819b38f05701555fbaf0b327f8d1245946682
alexkonkin@gmail.com <alexkonkin@gmail.com@b8128056-237a-151d-1e11-175cfe163b60>
2014-06-04 15:12:09 +0000

    java_script: initial commit of the tetris project

---
+++ trunk/java_script/strict_function.js
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: oleksiy.konkin
+ * Date: 4/18/14
+ * Time: 12:25 PM
+ * To change this template use File | Settings | File Templates.
+ */
+function sayHi(person) {
+
+    //"use strict";
+    //prompt ('your message',person);
+    message = "Привет, " + person; // error: message is not defined
+    alert (message);
+}
