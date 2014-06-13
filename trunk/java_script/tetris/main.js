f039139055e5c9fc7cc438cbc6f954edbc2540c6
alexkonkin@gmail.com <alexkonkin@gmail.com@b8128056-237a-151d-1e11-175cfe163b60>
2014-06-13 09:04:08 +0000

    java_script: keep working on figures generation

--- trunk/java_script/tetris/main.js
+++ trunk/java_script/tetris/main.js
@@ -28,14 +28,39 @@ function generateTable(){
         for (var column=0; column<10; column++){
             var td = document.createElement('TD');
             td.id = "td_"+row+"_"+column;
-            td.appendChild(document.createTextNode(row + "," + column));
+            //td.appendChild(document.createTextNode(row + "," + column));
             tr.appendChild(td);
         }
     }
     myTableDiv.appendChild(table);
     detectTableCoordinates();
-    generateObject();
+    <!-- this function starts the game with the very first figure -->
+    //startMovement();
 }
+
+function generateFiguresField(){
+    var myTableDiv = document.getElementById("figure");
+    var table = document.createElement('TABLE');
+
+    var tableBody = document.createElement('TBODY');
+    table.appendChild(tableBody);
+
+    for (var row=0; row<4; row++){
+        var tr = document.createElement('TR');
+        tableBody.appendChild(tr);
+
+        for (var column=0; column<4; column++){
+            var td = document.createElement('TD');
+            td.id = "td_"+row+"_"+column;
+            //td.appendChild(document.createTextNode(row + "," + column));
+            tr.appendChild(td);
+        }
+    }
+    myTableDiv.appendChild(table);
+    detectTableCoordinates();
+    //startMovement();
+}
+
 
 function detectTableCoordinates(){
     var tbl = document.getElementsByTagName("table")[0];
@@ -164,3 +189,100 @@ function testMovement(){
         }
     //}
 }
+
+/*
+Вот алгоритм поворота матрицы на 90 градусов по часовой стрелке. а-исходная матрица, b -"повернутая".
+    Код:
+
+
+for i:=1 to 4 do
+    for j:=1 to 4 do
+    b[i,j]:=a[n-j+1,i];
+*/
+
+var GameField = function (aRow, aColumn){
+    this.row = aRow;
+    this.column = aColumn;
+    var gameArray = [];
+    while(gameArray.push([]) < aRow);
+    for (var row=0; row < gameArray.length; row++){
+        for (var col=0; col < gameArray.length; col++)
+            gameArray[row].push(0);
+    }
+    // private constructor
+    console.log(gameArray);
+}
+
+var RandomFigure = function (){
+    this.position;
+    this.letter = "";
+    this.figureArray = [];
+
+    function getRandomInt(min, max) {
+        //return Math.floor(Math.random() * (max - min + 1)) + min;
+        // return I
+        return 1;
+    }
+
+    function defineFigureType(aNum){
+        switch (aNum) {
+            // letter I
+            case 1:
+                this.figureArray = [[0,1,0,0],[0,1,0,0],[0,1,0,0],[0,1,0,0]];
+                this.letter = "I";
+                this.position = 1;
+            break;
+            // letter J
+            case 2:
+                ;
+            break;
+            // letter L
+            case 3:
+                ;
+            break;
+            // letter S
+            case 4:
+                ;
+            break;
+            // letter T
+            case 5:
+                ;
+            break;
+            // letter S
+            case 6:
+                ;
+            break;
+            // letter Z
+            case 7:
+                ;
+            break;
+        }
+    }
+
+    this.figure = getRandomInt(1,7);
+    defineFigureType(this.figure);
+    console.log(this.letter);
+    console.log(this.figureArray);
+}
+
+function testFigureGeneration(){
+    //var gameField = GameField(4,4);
+    var aFigure = RandomFigure();
+}
+
+//"function(){ if (this.y > 1000000) { result = Math.floor(this.y / 1000000) + \"M\" }else if (this.y > 1000) { result = Math.floor(this.y / 1000) + \"K\" } return result; }"
+//plotOptions.series.dataLabels.formatter
+/*
+function formatValue() {
+    this.value = 10000000;
+    if (this.value > 1000000) {
+        result = Math.floor(this.value / 1000000) + "M"
+    }
+    else if (this.value > 1000) {
+        result = Math.floor(this.value / 1000) + "K"
+    }
+    return result;
+}
+
+console.log(formatValue(1000000));
+*/
